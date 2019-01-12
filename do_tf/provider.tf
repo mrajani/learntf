34fdc28bee29dd204cdef5e0a588d2089e707b92
mrajani <mahesh@ionosolutions.com>
2019-01-12 22:50:00 +0000

    tf for deploying two web server and one ha proxy load balancer ref: groovemonkey

--- do_tf/provider.tf
+++ do_tf/provider.tf
@@ -7,16 +7,3 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-connection {
-  user = "root"
-  type = "ssh"
-  private_key = "${file(var.pvt_key)}"
-  timeout = "2m"
-}
-
-provisioner "remote-exec" {
-  inline = [
-    "export PATH=$PATH:/usr/bin",
-    "sudo apt-get update && sudo apt-get install -y nginx"
-  ]
-}
