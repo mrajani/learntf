fd25348b1c5b7da0a25f1d0458f971e6f71f3b8b
mrajani <mahesh@ionosolutions.com>
2019-06-13 06:17:25 +0000

    bash script to install tfenv

---
+++ scripts/install_tfenv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update -yqq 
+sudo apt-get install -yqq unzip
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+
+if [ ! -d "$HOME/.local/bin" ] ; then
+    mkdir -p $HOME/.local/bin
+fi
+ln -s $HOME/.tfenv/bin/* $HOME/.local/bin/
+source $HOME/.profile
+which tfenv
+tfenv install latest
+tfenv install 0.11.14
