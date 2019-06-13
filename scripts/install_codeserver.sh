86a74f0366453951ac37500651f710d9539adc9a
mrajani <mahesh@ionosolutions.com>
2019-06-13 06:23:22 +0000

    install visual studio code running under browser, cdr/code-server

---
+++ scripts/install_codeserver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Install Code Server on Linux Platform 
+url_latest=https://github.com/cdr/code-server/releases/latest
+url_effective=$(curl -sSL -I -o /dev/null -w %{url_effective} ${url_latest})
+tag=${url_effective##*/}
+echo "Installing ${tag} version of codeserver"
+
+codeserver=code-server${tag}-linux-x64.tar.gz
+wget -q https://github.com/cdr/code-server/releases/download/${tag}/${codeserver}
+vsdir=vscodeserver
+[[ -d $vsdir ]] && rm -rf $vsdir
+mkdir $vsdir \
+&& tar zxf $codeserver -C $vsdir --strip-components 1 \
+&& rm $codeserver
