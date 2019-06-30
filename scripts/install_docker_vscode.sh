fb2f30dde21aef4c47f39256764a92f333da2987
mrajani <mahesh@ionosolutions.com>
2019-06-30 01:30:24 +0000

    rename install_docker_vscode to run_docker_vscode add name to docker container add --rm to docker run command

--- scripts/install_docker_vscode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-dir=${1:-.}
-echo home dir is ${PWD}/${dir}
-docker run -it -p 8443:8443 -v "${PWD}/${dir}:/home/coder/project" \
-	-v "/Scratch:/Scratch" codercom/code-server --no-auth
-
