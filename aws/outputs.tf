ee8e75e276115ee8e4f1f58e1d69c908899943af
mrajani <mahesh@ionosolutions.com>
2019-07-01 01:21:29 +0000

    modify to output list

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,11 +1,27 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 output "azs" {
   value = data.aws_availability_zones.available.names
 }
 
 output "eip_ip" {
-  value = [aws_eip.nateip.*.public_ip]
+  value = aws_eip.nateip.*.public_ip
 }
 
 output "natgw" {
-  value = [aws_nat_gateway.natgw.*.id]
+  value = aws_nat_gateway.natgw.*.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public.*.id
+}
+
+
+output "private_subnet_id" {
+  value = aws_subnet.private.*.id
+}
+
+output "db_subnet_id" {
+  value = aws_subnet.db.*.id
 }
