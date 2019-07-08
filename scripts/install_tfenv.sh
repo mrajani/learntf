9387ec0d79f644f2db80f0da0693f6ce584a333f
mrajani <mahesh@ionoinc.com>
2019-07-08 20:10:05 +0000

    install tfenv for amzn, cleanup script

--- scripts/install_tfenv.sh
+++ scripts/install_tfenv.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-sudo apt-get update -yqq 
-sudo apt-get install -yqq unzip
+if [[ ( -z ${os} && -z ${dist} ) ]]; then
+  if [ -e /etc/os-release ]; then
+    . /etc/os-release
+    os=$( echo ${ID} | awk '{ print tolower($1) }')
+    dist=${VERSION_ID}
+  fi
+fi
+echo "Found ${os} "
+
+if [[ ${os} == "centos" ]]; then
+  # Install Terraform in CentOS
+  echo   
+elif [[ ${os} == "amzn" ]]; then
+  # Install Terraform in Amzn Linux
+  echo Installing Terraform for AWS AMI   
+elif [[ ${os} == "ubuntu" ]]; then
+  # Install Docker in Ubuntu 18.04
+  sudo apt-get -yqq update && sudo apt-get install -y unzip
+else
+  echo "Cannot identify the OS"
+  exit 1;
+fi
+
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 
 if [ ! -d "$HOME/.local/bin" ] ; then
