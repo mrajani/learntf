e561298b45f457300f2c074718c662a5bef08530
mrajani <mahesh@ionosolutions.com>
2019-07-09 23:51:57 +0000

    create kms key and give it an alias, test ciphertext blob

---
+++ scratch/kms.tf
@@ -0,0 +1,36 @@
+resource "aws_kms_key" "this" {
+  description             = "KMS key for S3 encryption"
+  deletion_window_in_days = 10
+  is_enabled = true
+}
+
+resource "aws_kms_alias" "this" {
+  name          = "alias/s3-encrypt-key"
+  target_key_id = "${aws_kms_key.this.key_id}"
+}
+
+
+resource "aws_kms_ciphertext" "this" {
+  key_id = "${aws_kms_key.this.key_id}"
+  plaintext = "hello world" 
+}
+
+output "b1_key_arn" {
+  value = aws_kms_key.this.arn
+}
+
+output "b1_key_id" {
+  value = aws_kms_key.this.key_id
+}
+
+output "b2_targetkey_arn" {
+  value = aws_kms_alias.this.target_key_arn
+}
+
+output "b2_keyalias_arn" {
+  value = aws_kms_alias.this.arn
+}
+
+output "b3_ciphertext_blob" {
+  value = aws_kms_ciphertext.this.ciphertext_blob
+}
