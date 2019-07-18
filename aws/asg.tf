9e6896e5e90ac1c05e507af33d4062d877ba9d88
mrajani <mahesh@ionosolutions.com>
2019-07-18 05:59:42 +0000

    add second sg for ec2 in launch template

--- aws/asg.tf
+++ aws/asg.tf
@@ -31,3 +31,32 @@ resource "aws_security_group" "sg_lb" {
   tags = merge(var.tags, map("Name", format("%s-sg-lb", var.tags["Name"])))
 
 }
+
+resource "aws_security_group" "sg_lt" {
+  vpc_id      = aws_vpc.main.id
+  name        = format("%s-sg-lt", var.tags["Name"])
+  description = "SG for Ec2 Launch Template"
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.sg_lb.id}"]
+  }
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, map("Name", format("%s-sg-lt", var.tags["Name"])))
+
+}
