c6b0b0757c4bfd552b6f1b81bfd07546cc4e8fb9
mrajani <mahesh@ionosolutions.com>
2019-07-18 06:26:51 +0000

    add listen, target group

--- aws/asg.tf
+++ aws/asg.tf
@@ -43,10 +43,10 @@ resource "aws_security_group" "sg_lt" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = ["${aws_security_group.sg_lb.id}"]
   }
   # outbound internet access
@@ -60,3 +60,30 @@ resource "aws_security_group" "sg_lt" {
   tags = merge(var.tags, map("Name", format("%s-sg-lt", var.tags["Name"])))
 
 }
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = "${aws_lb.fe_lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.lb_tg.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "lb_tg" {
+  name     = "alb-target-group"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  tags     = merge(var.tags, map("Name", format("%s-sg-lt", var.tags["Name"])))
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 10
+  }
+}
+
