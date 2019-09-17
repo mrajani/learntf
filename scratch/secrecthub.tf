41156ead5c9a46677566efec6a9fc9719135c4d4
mrajani <mahesh@ionosolutions.com>
2019-09-17 10:20:02 -0700

    try out secret hub

---
+++ scratch/secrecthub.tf
@@ -0,0 +1,31 @@
+variable "secrethub_user" {
+
+}
+
+provider "secrethub" {
+  version    = "~> 0.2.0"
+  credential = "${file("~/.secrethub/credential")}"
+}
+
+resource "secrethub_secret" "db_password" {
+  path = "${var.secrethub_user}/start/db-password"
+
+  generate {
+    length      = 22
+    use_symbols = true
+  }
+}
+
+resource "secrethub_secret" "db_user" {
+  path  = "${var.secrethub_user}/start/db-user"
+  value = "mydbuser"
+}
+
+output "db_password" {
+  value = "${secrethub_secret.db_password.value}"
+}
+
+output "db_user" {
+  value = "${secrethub_secret.db_user.value}"
+}
+
