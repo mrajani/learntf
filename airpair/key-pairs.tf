596fdc8b7dbdafc6595a7598d7410ea7a5059208
mrajani <mahesh@ionosolutions.com>
2019-10-22 03:53:24 -0700

    delete airpair tf

--- airpair/key-pairs.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "${var.prefix}-key"
-  public_key = "${file("${var.key_pair_path}.pub")}"
-}
--- airpair/s3.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "main" {
-  bucket = "${var.prefix}-s3-olpcaswi"
-  acl    = "private"
-
-  tags = {
-    Name        = "${var.prefix}-s3"
-    Environment = "Dev"
-  }
-}
