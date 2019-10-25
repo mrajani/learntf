8734529800db408b7608c12da6e40d791dc0c9a0
mrajani <mahesh@ionosolutions.com>
2019-10-25 02:22:53 -0700

    delete remote state files

--- aws/s3backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "remote-tfstate-pqc1jhomr7lw"
-    key            = "sandbox/vpc/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tfstate-remotelock"
-  }
-}
