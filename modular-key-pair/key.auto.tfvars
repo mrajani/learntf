f83c760e173c69c9ac50f520594d705ff4877eaf
mrajani <laltopi@gmail.com>
2020-11-16 15:33:44 -0800

    clean up remove key-pair module

--- modular-key-pair/key.auto.tfvars
+++
@@ -1,3 +0,0 @@
-# public_key = "~/.ssh/MxR-KeyPair"
-gen_key_pair = true
-key_name = "k8s-key"
--- modular-key-pair/modules/ssh_key_pair/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "key_name" {}
-variable "key_name_prefix" {}
-variable "public_key" {}
-variable "generate" {}
-variable "key_file" {
-  default = "aws-ssh-key"
-}
-
